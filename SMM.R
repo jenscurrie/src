faf011d8a08197733f96ced797b54d9fde58d2af
Jens Currie <jenscurrie@pwf.local>
2017-10-28 08:43:01 -1000

    added plots, no trends obvious

--- SMM.R
+++ SMM.R
@@ -3,3 +3,6 @@ getwd()
 #add some more cool shit
 #code blah blah 
 #Solving extinction code...
+a<-read.csv("C:/Users/jenscurrie/Documents/rgit/myrepo/data/bbmroData.csv")
+plot(a$Depth,a$Count)
+plot(a$SST,a$Count)
